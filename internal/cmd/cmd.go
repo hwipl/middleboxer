9a1efb6cda7ad79a28be788874b4e40c0c9d5b5b
hwipl <33433250+hwipl@users.noreply.github.com>
2021-07-18 11:34:50 +0200

    add command line argument "server"
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- internal/cmd/cmd.go
+++ internal/cmd/cmd.go
@@ -1,4 +1,30 @@
 package cmd
 
+import (
+	"flag"
+)
+
+var (
+	// run as server?
+	serverMode = false
+)
+
+// parseCommandLine parses the command line arguments
+func parseCommandLine() {
+	// configure command line arguments
+	flag.BoolVar(&serverMode, "server", serverMode, "run as server")
+
+	// parse command line arguments
+	flag.Parse()
+
+}
+
+// Run is the main entry point
 func Run() {
+	parseCommandLine()
+
+	if serverMode {
+		newServer().run()
+		return
+	}
 }
