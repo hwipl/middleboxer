f1e16502f3b99e0f5ac1301b21a51f5ef8848355
hwipl <33433250+hwipl@users.noreply.github.com>
2021-07-19 10:48:10 +0200

    server: add listener to server struct
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- internal/cmd/server.go
+++ internal/cmd/server.go
@@ -1,14 +1,40 @@
 package cmd
 
+import (
+	"log"
+	"net"
+)
+
 // server stores information about a server
 type server struct {
+	listener net.Listener
+}
+
+// handleClient handles a client connection
+func (s *server) handleClient(client net.Conn) {
 }
 
 // run this server
 func (s *server) run() {
+	for {
+		client, err := s.listener.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+		go s.handleClient(client)
+	}
 }
 
 // newServer creates an new server
 func newServer() *server {
-	return &server{}
+	// create listener
+	listener, err := net.Listen("tcp", "")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// return server
+	return &server{
+		listener,
+	}
 }
