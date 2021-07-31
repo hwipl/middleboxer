35883fe4d4e2ad1f34520e239019532d39ff0b36
hwipl <33433250+hwipl@users.noreply.github.com>
2021-07-31 12:29:13 +0200

    receiver: add pcap
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/hwipl/middleboxer
 
 go 1.16
+
+require (
+	github.com/google/gopacket v1.1.17
+	github.com/hwipl/packet-go v0.0.0-20200629161832-32eaac89b0cc
+)
--- internal/cmd/receiver.go
+++ internal/cmd/receiver.go
@@ -1,14 +1,33 @@
 package cmd
 
+import (
+	"github.com/google/gopacket"
+	"github.com/hwipl/packet-go/pkg/pcap"
+)
+
 // receiver is a test in receiver mode
 type receiver struct {
 	test    *MessageTest
 	results chan *MessageResult
 }
 
+// HandlePacket handles a packet received via pcap
+func (r *receiver) HandlePacket(packet gopacket.Packet) {
+	// TODO: do something
+}
+
 // run runs the receiver
 func (r *receiver) run() {
-	// TODO: do something
+	// create listener
+	listener := pcap.Listener{
+		PacketHandler: r,
+		Device:        r.test.Device,
+	}
+
+	// prepare listener
+	listener.Prepare()
+
+	// TODO: start loop, inform server
 }
 
 // newReceiver creates a new test in receiver mode
