6a4af00abcc191284b5a1e9f87093d088effe4d4
hwipl <33433250+hwipl@users.noreply.github.com>
2021-08-03 13:06:15 +0200

    packet: add regs to packetListener
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- internal/cmd/packet.go
+++ internal/cmd/packet.go
@@ -5,10 +5,17 @@ import (
 	"github.com/hwipl/packet-go/pkg/pcap"
 )
 
+// packetListenerReg is a (de)register message for the packet listener
+type packetListenerReg struct {
+	add     bool
+	handler pcap.PacketHandler
+}
+
 // packetListener is a pcap packet listener
 type packetListener struct {
 	listener pcap.Listener
 	handlers []pcap.PacketHandler
+	regs     chan packetListenerReg
 	packets  chan gopacket.Packet
 }
 
@@ -22,6 +29,7 @@ func (p *packetListener) HandlePacket(packet gopacket.Packet) {
 func (p *packetListener) loop() {
 	for {
 		select {
+
 		case packet, more := <-p.packets:
 			// handle packets coming from pcap
 			if !more {
@@ -29,6 +37,12 @@ func (p *packetListener) loop() {
 			}
 			for _, h := range p.handlers {
 				h.HandlePacket(packet)
+			}
+
+		case reg, more := <-p.regs:
+			// handle packet handler (de)registrations
+			if !more {
+				return
 			}
 		}
 	}
@@ -38,6 +52,7 @@ func (p *packetListener) loop() {
 func newPacketListener(device string) *packetListener {
 	// create packet listener
 	p := &packetListener{
+		regs:    make(chan packetListenerReg),
 		packets: make(chan gopacket.Packet),
 	}
 
