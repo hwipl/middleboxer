ec445db262aa0e31ca428603a26b7fe361b5f2ea
hwipl <33433250+hwipl@users.noreply.github.com>
2021-08-11 10:57:33 +0200

    plan: add receiversReady to planItem
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- internal/cmd/plan.go
+++ internal/cmd/plan.go
@@ -7,6 +7,7 @@ type planItem struct {
 	id              uint32
 	senderMsg       *MessageTest
 	receiverMsg     *MessageTest
+	receiversReady  []uint8
 	senderResults   []*MessageResult
 	receiverResults []*MessageResult
 }
@@ -76,6 +77,17 @@ func (p *plan) handleResult(clientID uint8, result *MessageResult) {
 	if isSender {
 		item.senderResults = append(item.senderResults, result)
 	} else {
+		if result.Result == ResultReady {
+			// handle "ready" results
+			if listContainsID(item.receiversReady, clientID) {
+				log.Println("Double ready from client", clientID)
+				return
+			}
+			item.receiversReady = append(item.receiversReady, clientID)
+			return
+		}
+
+		// handle other results
 		item.receiverResults = append(item.receiverResults, result)
 	}
 }
