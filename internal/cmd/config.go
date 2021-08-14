04e457948aab6c49bb221dac126e095e47f0a5d2
hwipl <33433250+hwipl@users.noreply.github.com>
2021-08-14 17:29:45 +0200

    config: add ParseCommandLine() to Config
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- internal/cmd/config.go
+++ internal/cmd/config.go
@@ -1,5 +1,11 @@
 package cmd
 
+import (
+	"flag"
+	"log"
+	"math"
+)
+
 // Config contains the configuration
 type Config struct {
 	// ServerMode determines if we run as a server or a client
@@ -10,6 +16,25 @@ type Config struct {
 
 	// ClientID is the id of the client
 	ClientID uint8
+}
+
+// ParseCommandLine fills the config from command line arguments
+func (c *Config) ParseCommandLine() {
+	// configure command line arguments
+	flag.BoolVar(&c.ServerMode, "server", c.ServerMode,
+		"run as server (default: run as client)")
+	flag.StringVar(&c.ServerAddress, "address", c.ServerAddress,
+		"set address to connect to (client mode) or listen on (server mode)")
+	cid := flag.Uint("id", 0, "set id of the client")
+
+	// parse command line arguments
+	flag.Parse()
+
+	// set client id
+	if *cid > math.MaxUint8 {
+		log.Fatal("invalid client id")
+	}
+	c.ClientID = uint8(*cid)
 }
 
 // NewConfig creates a new Config
