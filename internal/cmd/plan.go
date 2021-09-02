0771fd604910625d36002e598660d7251fb48f4f
hwipl <33433250+hwipl@users.noreply.github.com>
2021-09-02 16:16:41 +0200

    plan: remove passesString() from planResults
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- internal/cmd/plan.go
+++ internal/cmd/plan.go
@@ -24,23 +24,6 @@ type planResults struct {
 	others    map[uint16]*planResult
 }
 
-// passesString returns a string for mostly passing results
-func (p *planResults) passesString() string {
-	if len(p.rejects) > len(p.passes) || len(p.drops) > len(p.passes) {
-		return ""
-	}
-	s := fmt.Sprintf("%d:%d policy PASS\n", p.firstPort, p.lastPort)
-	for i := p.firstPort; i <= p.lastPort && i != 0; i++ {
-		if r, ok := p.rejects[i]; ok {
-			s += fmt.Sprintf("%d\treject\n", r.port)
-		}
-		if r, ok := p.drops[i]; ok {
-			s += fmt.Sprintf("%d\tdrop\n", r.port)
-		}
-	}
-	return s
-}
-
 // rejectsString returns a string for mostly rejected results
 func (p *planResults) rejectsString() string {
 	if len(p.passes) >= len(p.rejects) || len(p.drops) > len(p.rejects) {
